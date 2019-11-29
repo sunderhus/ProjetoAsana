1a5975ddd114959327ed7c3d170d60119de37df0
Igor Cemim <igor@cemim.com.br>
2019-11-29 01:39:15 -0300

    Correções navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   title: string = 'Gerenciador de Propostas';
+  navbarOpen = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
 }
